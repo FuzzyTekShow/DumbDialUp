be820c4b2eca5e5f47464ac8b958576c4e8d8c5e
Fuzzy Mannerz <fuzzymannerz@gmail.com>
2022-01-21 20:22:10 +0100

    init commit

---
+++ DumbDialUp/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Resources;
+
+namespace DumbDialUp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        NetworkController networkController;
+
+        // Close window button stuff
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitGUI();
+            InitSound();
+        }
+
+        /// <summary>
+        /// Initialise the GUI
+        /// </summary>
+        private void InitGUI()
+        {
+            // Setting the network controller will DISABLE the network connection.
+            networkController = new NetworkController();
+            ToggleText();
+        }
+
+        /// <summary>
+        /// Start the famous dialup sounds
+        /// </summary>
+        private void InitSound()
+        {
+            StreamResourceInfo sri = Application.GetResourceStream(new Uri("pack://application:,,,/DumbDialUp;component/dialup.wav"));
+
+            if (sri != null)
+            {
+                SoundPlayer player = new SoundPlayer(sri.Stream);
+                player.Load();
+                player.Play();
+            }
+        }
+
+
+        private async void ToggleText()
+        {
+            tbWaitText.Text = "Please wait.\nDialing";
+            int waitTime = 500;
+
+            for (int i = 0; i < 3; i++)
+            {
+                tbStatusText.Text = string.Empty;
+                await Task.Delay(waitTime);
+                tbStatusText.Text = ".";
+                await Task.Delay(waitTime);
+                tbStatusText.Text = "..";
+                await Task.Delay(waitTime);
+                tbStatusText.Text = "...";
+                await Task.Delay(waitTime);
+            }
+
+            tbWaitText.Text = "Please wait.\nEstablishing connection";
+
+            for (int i = 0; i < 9; i++)
+            {
+                tbStatusText.Text = string.Empty;
+                await Task.Delay(waitTime);
+                tbStatusText.Text = ".";
+                await Task.Delay(waitTime);
+                tbStatusText.Text = "..";
+                await Task.Delay(waitTime);
+                tbStatusText.Text = "...";
+                await Task.Delay(waitTime);
+            }
+
+            if (networkController != null)
+            {
+                networkController.Enable();
+            }
+            
+            tbWaitText.FontWeight = FontWeights.Bold;
+            tbWaitText.Text = "\nConnection established!\n";
+            pbProgress.Visibility = Visibility.Hidden;
+            tbStatusText.Text = string.Empty;
+            await Task.Delay(3250);
+            Application.Current.Shutdown();
+        }
+
+
+        /// <summary>
+        /// Be sure to RE-ENABLE connection on window close!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (networkController != null)
+            {
+                networkController.Enable();
+            }
+        }
+
+
+        /// <summary>
+        /// Called as soon as the window is loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+        }
+
+
+        // Window close button stuff
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    }
+}
---
+++ DumbDialUp/NetworkController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumbDialUp
+{
+    public class NetworkController
+    {
+
+        private NetworkInterface nw;
+        
+        public NetworkController()
+        {
+            // Get the adaptor in use
+            nw = GetActiveNetworkInterface();
+
+            // Disable it
+            if (nw != null)
+            {
+                Disable();
+            }
+        }
+
+        /// <summary>
+        /// Gets the active network connection
+        /// </summary>
+        /// <returns></returns>
+        private NetworkInterface GetActiveNetworkInterface()
+        {
+            NetworkInterface adapter = null;
+
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Ignore tunnel and loopback devices
+                if (ni.OperationalStatus == OperationalStatus.Up &&
+                    ni.NetworkInterfaceType != NetworkInterfaceType.Tunnel &&
+                    ni.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                {
+                    adapter = ni;
+                }
+            }
+            return adapter;
+        }
+
+
+        public void Enable()
+        {
+            ProcessStartInfo psi =
+                   new ProcessStartInfo("netsh", "interface set interface \"" + nw.Name + "\" enable");
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            Process p = new Process{StartInfo = psi};
+            p.Start();
+        }
+
+        public void Disable()
+        {
+            ProcessStartInfo psi =
+                new ProcessStartInfo("netsh", "interface set interface \"" + nw.Name + "\" disable");
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            Process p = new Process{StartInfo = psi};
+            p.Start();
+        }
+    }
+}
